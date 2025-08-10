f1457e509a131f40467fd7f860180d6284c50494
FerNandoGD24 <fernando.gatica.dominguez@outlook.com>
2025-08-09 22:24:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,1 @@
-sudo mv APYAA.sh ~/.config
-sudo mv servicio.service /etc/systemd/system/
-cd ~/.config
-sudo chmod +x APYAA.sh
-sudo systemctl daemon-reload
-sudo systemctl enable servicio.service
-cd
-rm -fdr APYAA
+sudo mv yay-shutdown.service /etc/systemd/system/
