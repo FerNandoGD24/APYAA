7c0a4c7b06c39da1cf18dc465eecf96ac77de63e
FerNandoGD24 <fernando.gatica.dominguez@outlook.com>
2025-08-09 22:26:17 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ sudo mv yay-shutdown.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable yay-shutdown.service
 sudo systemctl start yay-shutdown.service
+sudo systemctl status yay-shutdown.service
+cd
+rm -fdr APYAA
